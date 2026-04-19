acfba64e9864c8e150cd50fe751f2263939dcd9e
itsYuuuka <okan_altun@hotmail.com>
2026-04-19 13:09:30 +0200

    add template.html, content/index.md, generate_page function, and update main.sh

--- src/generate_page.py
+++ src/generate_page.py
@@ -1,3 +1,21 @@
+import os
+from markdown_to_html import markdown_to_html_node
+
+
+def generate_page(from_path, template_path, dest_path):
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+    with open(from_path) as f:
+        markdown = f.read()
+    with open(template_path) as f:
+        template = f.read()
+    html = markdown_to_html_node(markdown).to_html()
+    title = extract_title(markdown)
+    page = template.replace("{{ Title }}", title).replace("{{ Content }}", html)
+    os.makedirs(os.path.dirname(dest_path), exist_ok=True)
+    with open(dest_path, "w") as f:
+        f.write(page)
+
+
 def extract_title(markdown):
     for line in markdown.split("\n"):
         if line.startswith("# "):
--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 from copy_static import copy_static
+from generate_page import generate_page
 
 
 def main():
     copy_static("static", "public")
+    generate_page("content/index.md", "template.html", "public/index.html")
 
 
 main()
